8978e703695a4a6ec3223c6a9545aecede83a221
Kabilajayan <144537378+Kabilajayan@users.noreply.github.com>
2023-10-11 12:04:18 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,23 +1,14 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
-
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-    return sorted_students
-
-# Example usage:
-students_list = [
-    Student("TITAN", "22G233", 7.7),
-    Student("Luffy", "L12AZ", 5.7),
-    Student("Zoro", "Z789", 6.7),
-    # Add more students as needed
-]
-
-sorted_students = sort_students(students_list)
-
-# Display sorted students
-for student in sorted_students:
-    print(f"{student.name}, {student.roll_number}, CGPA: {student.cgpa}")
+#Challenge 2.2
+class Player:
+  def play(self):
+    print("The Player is playing Cricket")
+class Batsman(Player):
+  def play(self):
+    print("The Batsman is Batting ")
+class Bowler(Player):
+  def play(self):
+    print("The Bowler is Bowling ")
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
