cdad2bba5efbb8f0b697dbda5c62096fa0e38dc1
Kabilajayan <144537378+Kabilajayan@users.noreply.github.com>
2023-10-11 11:22:14 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,16 +1,30 @@
-def linear_search_product(product_list, target_product):
-  indices = []
+#Challenge2.1
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-  for index, product in enumerate(product_list):
-    if product == target_product:
-      indices.append(index)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print("Deposited Rs{}. New balance: Rs{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid deposit amount.")
 
-  return indices
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print("Withdraw Rs{}. New balance: Rs{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
 
+    def display_balance(self):
+        print("Account balance for {} (Account #{}): Rs{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
 
-# Example usage:
-products = ["apple", "banana", "orange", "apple", "grape"]
-target_product = "apple"
+account = BankAccount(account_number="2412200407", account_holder_name="TITAN", initial_balance=7000.0)
 
-result = linear_search_product(products, target_product)
-print(f"Indices of {target_product}: {result}")
+account.display_balance()
+account.deposit(780.0)
+account.withdraw(3.0)
+account.display_balance()
